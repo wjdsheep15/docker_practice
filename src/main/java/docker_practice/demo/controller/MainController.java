56d5d80e778c445f85d434282ce5776a490b1534
wjdsheep15 <wjd15sheep@gmail.com>
2025-02-24 22:13:17 +0900

    Feat: gitinit

---
+++ src/main/java/docker_practice/demo/controller/MainController.java
@@ -0,0 +1,16 @@
+package docker_practice.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        String name = "seung";
+        int age = 20;
+        return name + " is " + age + " years old";
+    }
+
+}
